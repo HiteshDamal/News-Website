9cf904ccc22fc7d08070045a8b05077c7cbd9983
HiteshDamal <88076630+HiteshDamal@users.noreply.github.com>
2023-08-28 13:14:12 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -3,7 +3,7 @@ import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
-import Search from './Search';
+
 
 
 const News = (props) => {
